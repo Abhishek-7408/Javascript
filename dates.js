7ae4ca7d3c8433b34d519accf9b86743ab38a9fd
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-11-23 18:47:06 +0530

    final 1st module

---
+++ dates.js
@@ -0,0 +1,40 @@
+//Dates
+
+
+let myDate = new Date()
+
+console.log(myDate.toISOString());
+
+console.log(myDate.toJSON());
+
+console.log(myDate.toDateString());
+console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2024, 0,23)
+console.log(myCreatedDate.toDateString());
+
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp); // mili sec\\
+
+
+console.log(myCreatedDate.getTime()); 
+
+console.log(myCreatedDate.getTime()); 
+
+console.log(Math.floor(Date.now()/1000))
+
+let newdate = new Date();
+console.log(newdate.getMonth());
+
+
+
+
+
+
+
+
+
