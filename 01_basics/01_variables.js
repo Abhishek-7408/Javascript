2266370ccbdcc5e7330ee21538ffe9a35f2a1d8d
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-11-23 13:39:18 +0530

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accouuntId = 144553
+let accountEmail = "abhgishek@gmail.com"
+var accountPass = "123456"
+accountCity = "Gorakhpur"
+
+
+// accouuntId = 2
+
+accountEmail = "hc@gmail.com"
+accountPass = "2131213"
+accountCity = "Bengluru"
+
+
+let accountState
+
+console.log(accouuntId);
+console.log(accountEmail);
+
+console.table([accouuntId,accountState, accountEmail, accountPass,accountCity])
+
+/*
+preffer not to use to var because of block scope
+
+
+*/
---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js in newer js
+
+
+// alert("hello") we are using node js not browser
+
+console.log(3+3);   
+
+
+console.log("abhishek");
+
+
+let name = "abhishek"
+let age = 19
+
+let isLoogedIn = false
+
+
+
+
+
+
