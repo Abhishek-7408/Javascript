ff1555e33c141d751888aad9c6d7cc58061e37dc
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2024-11-23 14:24:17 +0530

    conversion of dataTyppes

---
+++ 01_basics/conversionOperat.js
@@ -0,0 +1,32 @@
+let score = "13abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber);
+
+// console.log(typeof(score));
+
+console.log(typeof valueInNumber);
+
+
+
+let isLoggedIn = 1
+
+let booleanISLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanISLoggedIn);
+
+
+let someNumbers = 33
+let stringNumber = String(someNumbers)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
